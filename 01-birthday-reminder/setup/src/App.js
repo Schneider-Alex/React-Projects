6fab954e8ff6661262aae6e9328dbb8cca9b5d04
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-07-26 12:05:43 -0400

    Testing set up

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  <h1>Hello</h1>
+  return <h2>reminder project setup</h2>
+  ;
+
 }
 
 export default App;
